775b870a28d25b47de02a3707b90416e2d0ecd89
ranaivo.it <ranaivo.it@gmail.com>
2022-08-22 21:13:26 +0300

    Mise a jours site controller

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,24 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Site;
+use App\Repository\SiteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SiteController extends AbstractController
 {
-    #[Route('/site', name: 'url_site')]
-    public function index(): Response
+    #[Route('/sites', name: 'sites')]
+    public function index(SiteRepository $repo): Response
     {
         return $this->render('site/index.html.twig', [
             'title' => 'Sites',
+            'sites' => $repo->findAll()
         ]);
     }
-    #[Route('/site/add', name: 'url_site_add')]
+    
+    #[Route('/sites/add', name: 'sites_add')]
     public function add(): Response
     {
         return $this->render('site/add.html.twig', [
             'title' => 'Sites',
+        ]);
+    }
+
+    #[Route('/sites/{id}', name: 'sites_show')]
+    public function show($id, SiteRepository $repo): Response
+    {
+        return $this->render('site/show.html.twig', [
+            'title' => 'Sites',
+            'site' => $repo->find($id)
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,12 +15,16 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr-FR');
 
         //Data fixture Site
+        $categories = array("CHU", "CHR", "CSB II", "Manarapenitra", "Dispensaire");
 
         for ($i=0; $i < 5; $i++) { 
             $site = new Site();
-            
+            $site->setNom($faker->company())
+                ->setCategorie($categories[mt_rand(0, 4)])
+                ->setImage($faker->imageUrl());
+            $manager->persist($site);
         }
-
+       
         $manager->flush();
     }
 }
